cd37fc710df9b85e8b8181264c24cfcfd76a9391
redphx <96280+redphx@users.noreply.github.com>
2023-05-02 18:42:06 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+long_desc = open("README.md").read()
+required = ['requests']
+
+setup(
+    name='APIxoo',
+    version="0.1.0",
+    author="redphx",
+    license="MIT",
+    url="https://github.com/redphx/apixoo",
+    download_url="https://github.com/redphx/apixoo",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    description="Python wrapper for Divoom Pixoo server API",
+    packages=[
+        "apixoo"
+    ],
+    include_package_data=True,
+    install_requires=required,
+    platforms="any",
+    keywords=[
+        "apixoo",
+        "divoom",
+        "pixoo",
+        "pixoo64"
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        "Programming Language :: Python"
+    ],
+    python_requires=">3.6",
+)
